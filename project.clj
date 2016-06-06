d3e4cd016e2cef9577ebe988d7fb966d9ca4b6a7
Anton Maklakov <anton.maklakov@vincit.com>
2016-06-06 07:16:41 +0300

    Bump version to 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject vincit/curd "0.1.1"
+(defproject vincit/curd "0.1.2"
   :description "Easy and sweet crud without hassle"
   :url "https://github.com/vincit/curd"
   :license {:name "Eclipse Public License"
