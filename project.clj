a8faa2cbda8b362b2863914f544007b4031bc82a
Anton Maklakov <anton.maklakov@vincit.com>
2017-09-27 09:18:28 +0300

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta1"]
                  [org.clojure/spec.alpha "0.1.123"]
-                 [org.clojure/java.jdbc "0.7.0-alpha3"]
+                 [org.clojure/java.jdbc "0.7.1"]
                  [camel-snake-kebab "0.4.0"]
                  [io.aviso/pretty "0.1.33"]]
   :profiles {:dev {:dependencies [[ragtime "0.6.3"]
-                                  [org.postgresql/postgresql "9.4.1210.jre7"]]
+                                  [org.postgresql/postgresql "42.1.4"]]
                    :plugins [[com.jakemccrary/lein-test-refresh "0.16.0"]]}}
   :test-refresh {:changes-only true})
